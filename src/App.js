499d2b0873e35e4f69d1dd8fd410860ff99b0606
salaveti.Reddy <6o1QLc5hi1blT0MvrtASl0e9nsFoWLJOL1J5xWSNH7ZaYfv0ReAbJQQJ99AKACAAAAAq259BAAASAZDOFcvr>
2025-08-13 17:30:20 +0530

    Counter Files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	return (
+		<div className="App">
+			<Counter />
+		</div>
+	);
 }
 
-export default App;
+function Counter() {
+	const [step, setStep] = useState(1);
+	const [count, setCount] = useState(0);
+	const date = new Date();
+	date.setDate(date.getDate() + count);
+
+	return (
+		<React.Fragment>
+			<div className="p">
+				<button className="m" onClick={() => setStep((s) => s - 1)}>
+					-
+				</button>
+				<span>step: {step}</span>
+				<button className="m" onClick={() => setStep((s) => s + 1)}>
+					+
+				</button>
+			</div>
+			<div className="p">
+				<button className="m" onClick={() => setCount((c) => c - step)}>
+					-
+				</button>
+				<span>Count: {count}</span>
+				<button className="m" onClick={() => setCount((c) => c + step)}>
+					+
+				</button>
+			</div>
+			<p className="p">
+				<span>
+					{count === 0
+						? "Today is "
+						: count > 0
+						? `${count} days from today is `
+						: `${Math.abs(count)} days ago was `}
+				</span>
+				<span>{date.toDateString()}</span>
+			</p>
+		</React.Fragment>
+	);
+}
